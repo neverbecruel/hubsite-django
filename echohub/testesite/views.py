00d94b1cd69b90326e0745b6691356c0d6e1edaf
Victor <pipoca.play19@gmail.com>
2024-02-24 15:23:00 -0300

    updates

--- echohub/testesite/views.py
+++ echohub/testesite/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 from django.template import loader
 from .models import Question
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from .models import Choice, Question
 
 
 
@@ -13,16 +17,36 @@ def index(request):
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id) # model e ORM
+    perguntas = question.choice_set.all()
+    if perguntas:
+        for choice in question.choice_set.all():
+            print(choice.choice_text)
+    else:
+        print("ERRO")
+
     return render(request, "testesite/detail.html",{"question": question})
 
 
 def results(request, question_id):
     return HttpResponse(f"Você está olhando para o resultado da questão {question_id}")
 
+
 def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
     try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Questão não encontrada.")
-    return render(request, "testesite/vote.html", {"question": question}) # request, template path
-                                                                                              # e contexto
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request,"testesite/detail.html",{
+                "question": question,
+                "error_message": "Você não selecionou uma questão.",
+            },
+        )
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("results", args=(question.id,)))
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "testesite/results.html", {"question": question})
